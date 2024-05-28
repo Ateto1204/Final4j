f0ae2ed0de72af39d4db4547df3abc7d789736b4
ZIhan <73235000+zihan0221@users.noreply.github.com>
2024-05-28 13:28:13 +0800

    Create apikey.java

---
+++ src/main/java/poateto/final4j/UseCases/apikey.java
@@ -0,0 +1,8 @@
+package poateto.final4j.UseCases;
+
+public class apikey {
+
+    public static final String OPENAI_API_KEY = System.getenv("demo");
+
+    public static final String RAPID_API_KEY = System.getenv("demo");
+}
