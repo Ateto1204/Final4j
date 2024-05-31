f8f22707af0cc6235e9e55eac0d05ad5c56bd291
ZIhan <73235000+zihan0221@users.noreply.github.com>
2024-05-31 14:26:35 +0800

    Update ApiKeySet.java

--- src/main/java/poateto/final4j/UseCases/Components/ApiKeySet.java
+++ src/main/java/poateto/final4j/UseCases/Components/ApiKeySet.java
@@ -4,5 +4,5 @@ public class ApiKeySet {
 
     public static final String OPENAI_API_KEY = "demo";
 
-    public static final String COHERE_API_KEY = "demo";
+    public static final String COHERE_API_KEY = "suaQjr6kh046zhZNj6tvxE43dPAGCKJpxyXm38Eg";
 }
