00884b10584fa4a23c3c971acce650b931eb968c
Ateto1202 <tonysu1204@gmail.com>
2024-06-02 21:41:38 +0800

    Design the language model selection algorithm

--- src/main/java/poateto/final4j/UseCases/Service/UserUseCase.java
+++ src/main/java/poateto/final4j/UseCases/Service/UserUseCase.java
@@ -7,7 +7,7 @@ import java.util.concurrent.ExecutionException;
 
 public interface UserUseCase {
     User saveUser(UserCreate user) throws ExecutionException, InterruptedException;
-    User getUserByEmail(String email) throws ExecutionException, InterruptedException;
+    User getUserByEmail(UserLogin user) throws ExecutionException, InterruptedException;
     String notifyModel(UserModifyWeight myModify) throws ExecutionException, InterruptedException;
     LMMessage sendMessage(UserMessage userMessage) throws ExecutionException, InterruptedException;
     boolean checkPwd(String email, String pwd) throws ExecutionException, InterruptedException;
